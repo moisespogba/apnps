696e165a47bc3176a0e03e91b7c1f1b6915ef972
moisesavila <pogavila00@gmail.com>
2020-11-18 21:08:41 -0300

    criação da rota layout

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express()
 app.get('/',function(req,res){
     res.send("ola moises")
 })
+app.get('/layout',function(req,res){
+    res.send("esse é o novo layout")
+})
 app.listen(4000,function(){
     console.log("conexão inicializada")
 })
