296b2fe19e8c89fb67c230a53244aba9e120fdd1
moisesavila <pogavila00@gmail.com>
2020-11-18 21:02:29 -0300

    adicionando a rota sobre

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express()
 app.get('/',function(req,res){
     res.send("ola moises")
 })
+app.get('/mensagem',function(req,res){
+    res.send("essa mensagem é automatica")
+})
+app.get('/sobre',function(req,res){
+    res.send("esta pagina esta sendo desenvolvida por moises")
+})
 app.listen(4000,function(){
     console.log("conexão inicializada")
 })
